74a5e1ebe44d1f62b8f876cbc7fe3c138f5aa3de
Miller <windymiller3@yahoo.co.uk>
2023-11-09 10:14:54 +0000

    Added dummy json to mimic retrieving a list of songs

--- resources/js/Pages/SearchPlaylist.vue
+++ resources/js/Pages/SearchPlaylist.vue
@@ -1,19 +1,36 @@
 <script setup>
+import { ref } from 'vue';
 import SearchPlaylistCards from '@/Widgets/SearchPlaylistCards.vue';
 import MainLayout from '@/Layouts/MainLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+// Variables
+// Save selected station
+let selectedStation = ref('');
+// Save an array of songs with titles and artist/s
+let songs = ref([]);
+
+const fetchPlaylist = async () => {
+    const response = await fetch(`./data/${selectedStation.value}.json`);
+    const data = await response.json();
+    songs.value = data.Radio1Dance;
+}
+
+
 </script>
+
 
 <template>
     <Head title="Search" />
 
     <MainLayout>
         <div class="container">
-            <form>
-                <select class="form-select form-select-lg mb-3 text-center" aria-label="Large select example">
+            <form @submit.prevent="fetchPlaylist">
+                <select v-model="selectedStation" class="form-select form-select-lg mb-3 text-center"
+                    aria-label="Large select example">
                     <option disabled selected>Select a Radio Station</option>
                     <option value="1">Radio 1</option>
-                    <option value="2">Radio 1 Dance</option>
+                    <option value="radio_1_dance">Radio 1 Dance</option>
                     <option value="3">Radio 1 Relax</option>
                     <option value="4">Radio 1Xtra</option>
                     <option value="5">Radio 2</option>
@@ -28,10 +45,14 @@ import { Head } from '@inertiajs/vue3';
                 </div>
             </form>
         </div>
-
-        <div class="container">
-            <SearchPlaylistCards v-for="n in 10" :key="n" />
-        </div>
+        <ul>
+            <li v-for="song in songs" :key="song.id">
+                {{ song.title }} by {{ song.artist }}
+            </li>
+        </ul>
+        <!-- <div class="container">
+            <SearchPlaylistCards v-for="n in 5" :key="n" />
+        </div> -->
 
         <div class="row">
             <div class="col-md-3 offset-md-5">
@@ -46,5 +67,9 @@ import { Head } from '@inertiajs/vue3';
 #add-to-spotify {
     background-color: rgb(5, 180, 34);
     border-color: rgb(5, 180, 34);
+}
+
+#add-to-spotify:hover {
+    opacity: 70%;
 }
 </style>
